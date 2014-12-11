40379c925988dd7aec19cfef4849d3708e81c5f1
em-ak <emilien.ak@gmail.com>
2014-12-11 16:26:40 +0100

    l'utilisateur peut voir les informations de l'enfant sur l'acceuil

--- client/helpers/home.js
+++ client/helpers/home.js
@@ -3,3 +3,9 @@ Template.home.helpers({
     return Parents.findOne({createdBy: Meteor.userId()});
   }
 })
+
+Template.home.helpers({
+  enfants: function(){
+    return Enfants.findOne({createdBy: Meteor.userId()});
+  }
+})
