4fef8fddc2b26f48e7ea548f065c982f87f14c55
balibou <benjamin.cherion@gmail.com>
2014-12-15 18:28:03 +0100

    Quand l'utilisateur créé un compte, il recoit un mail pour vérifier son compte mail

--- client/helpers/home.js
+++ client/helpers/home.js
@@ -2,10 +2,24 @@ Template.home.helpers({
   parents: function(){
     return Parents.findOne({createdBy: Meteor.userId()});
   }
-})
+});
 
 Template.home.helpers({
   enfants: function(){
     return Enfants.findOne({createdBy: Meteor.userId()});
   }
-})
+});
+
+Template.Homepage.created = function() {
+  if (Accounts._verifyEmailToken) {
+    Accounts.verifyEmail(Accounts._verifyEmailToken, function(err) {
+      if (err != null) {
+        if (err.message = 'Verify email link expired [403]') {
+          console.log('Sorry this verification link has expired.')
+        }
+      } else {
+        console.log('Thank you! Your email address has been confirmed.')
+      }
+    });
+  }
+};
--- server/smtp.js
+++ server/smtp.js
@@ -1,31 +1,33 @@
+// server/smtp.js
 Meteor.startup(function () {
-  smtp = {
-    username: 'server@gentlenode.com',   // eg: server@gentlenode.com
-    password: '3eeP1gtizk5eziohfervU',   // eg: 3eeP1gtizk5eziohfervU
-    server:   'mail.gandi.net',  // eg: mail.gandi.net
-    port: 25
-  }
+  // smtp = {
+  //   username: 'your_username',   // eg: server@gentlenode.com
+  //   password: 'your_password',   // eg: 3eeP1gtizk5eziohfervU
+  //   server:   'smtp.gmail.com',  // eg: mail.gandi.net
+  //   port: 25
+  // }
 
-  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
-
+  //process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+  process.env.MAIL_URL = 'smtp://postmaster%40sandbox92263dd139194b3fb953178ae8927d7d.mailgun.org:2090a2aa2b6e972534e532786161cccb@smtp.mailgun.org:587';
   // By default, the email is sent from no-reply@meteor.com. If you wish to receive email from users asking for help with their account, be sure to set this to an email address that you can receive email at.
-  Accounts.emailTemplates.from = 'Gentlenode <no-reply@gentlenode.com>';
+  Accounts.emailTemplates.from = 'Wild Code School <no-reply@wildcodeschool.fr>';
 
   // The public name of your application. Defaults to the DNS name of the application (eg: awesome.meteor.com).
-  Accounts.emailTemplates.siteName = 'Gentlenode Studio';
+  Accounts.emailTemplates.siteName = 'Wild Code School';
 
   // A Function that takes a user object and returns a String for the subject line of the email.
   Accounts.emailTemplates.verifyEmail.subject = function(user) {
-    return 'Confirm Your Email Address';
+    return 'Confirmez votre adresse email';
   };
 
   // A Function that takes a user object and a url, and returns the body text for the email.
   // Note: if you need to return HTML instead, use Accounts.emailTemplates.verifyEmail.html
   Accounts.emailTemplates.verifyEmail.text = function(user, url) {
-    return 'click on the following link to verify your email address: ' + url;
+    return 'Veuillez cliquer sur ce lien pour vérifier votre email: ' + url;
   };
 });
 
+// (server-side)
 Accounts.onCreateUser(function(options, user) {
   user.profile = {};
 
